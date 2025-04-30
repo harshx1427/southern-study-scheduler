74645f4211cc130c8c6530631eaed6a89601b419
Hualin120 <hualin120@gmail.com>
2025-04-30 13:35:41 -0400

    editing config.py

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
 
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
