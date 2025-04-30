c89d0a41e582be1072350d40314cb5ca0d945adf
Hualin120 <hualin120@gmail.com>
2025-04-30 14:17:55 -0400

    updating config.py

--- config.py
+++ config.py
@@ -2,9 +2,16 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///study_scheduler.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
 
 
 
