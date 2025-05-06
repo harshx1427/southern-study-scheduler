dc980224dd885d9489b233273415a548b1bbde80
Hualin Li <hualin120@gmail.com>
2025-05-05 20:11:03 -0400

    Add a function to send a message after the user creates an account.

--- app/__init__.py
+++ app/__init__.py
@@ -7,19 +7,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-<<<<<<< HEAD
 mail = Mail()
-=======
-
->>>>>>> 804539b0ac6863f041cfe1e56a32cebe161fa771
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models.models import User
-    return User.query.get(int(user_id))
 
 def create_app(config_object=DevelopmentConfig):
     app = Flask(__name__, instance_relative_config=True)
@@ -32,7 +23,7 @@ def create_app(config_object=DevelopmentConfig):
 
     from app.routes.auth import auth_bp
     from app.routes.main import main_bp
-    
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
 
@@ -41,4 +32,7 @@ def create_app(config_object=DevelopmentConfig):
 
     return app
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models.models import User
+    return User.query.get(int(user_id))
